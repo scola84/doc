7fa48d5f775518111b66ab11e651341b64a3e26f
M.C. van den Boogaart <scola@kvdb.name>
2019-07-12 12:33:03 +0200

    Set worker log

--- src/helper/setup.js
+++ src/helper/setup.js
@@ -1,9 +1,15 @@
 import {
+  Worker
+} from '@scola/worker';
+
+import {
   MessageSender,
   MysqlBuilder
 } from '../worker';
 
 export function setup() {
+  Worker.setLog(Worker.log);
+
   MessageSender.setup();
   MysqlBuilder.setup();
 }
